83ffa68c4946b97393a9333adcd271382295d081
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-05-08 19:53:12 +0800

    register function ready but not tested yet

--- src/components/Cards/CardRegister.vue
+++ src/components/Cards/CardRegister.vue
@@ -3,6 +3,8 @@
 import BaseInput from '@/components/UI/BaseInput.vue'
 import BaseButton from '@/components/UI/BaseButton.vue'
 import { reactive, ref } from 'vue'
+import { store } from '@/stores/index.js'
+import router from '@/router/index.js'
 const signUpData = reactive({
   fullName : "",
   username : "",
@@ -36,31 +38,47 @@ const confirmationPasswordCheck = () => {
   confirmPasswordSuccess.value = "block"
   confirmPasswordFailed.value = "none"
 }
+const register = async () => {
+  if (
+    signUpData.password !== signUpData.confirmPassword || signUpData.password < 8
+  ) {
+    signUpData.confirmPassword = ""
+    signUpData.password = ""
+    confirmPasswordFailed.value = "none"
+    confirmPasswordSuccess.value = "none"
+  } else {
+    await store.dispatch("auth/getRegisterData", signUpData)
+    await router.push("/")
+  }
+}
 </script>
 
 <template>
-  <div class="mt-4 d-flex flex-column gap-3">
-    <BaseInput v-model="signUpData.fullName" title-input="Full Name" identity="fullName" type="text" placeholder="enter your full name..." />
-    <BaseInput v-model="signUpData.username" title-input="Username" identity="Username" type="username" placeholder="enter your username..." />
-    <BaseInput v-model="signUpData.email" title-input="Email" identity="email" type="email" placeholder="enter your email..." />
-    <div class="">
-      <BaseInput @keyInput="passwordCheck" v-model="signUpData.password" title-input="Password" identity="password" type="password" placeholder="enter your password..." />
-      <p class="text-danger mt-1 fw-medium text-sm" :style="{display: passwordStatusDisplay}">
-        The Password field must be at least 8 character
-      </p>
+  <form action="" @submit.prevent="register">
+    <div class="mt-4 d-flex flex-column gap-3">
+      <BaseInput v-model="signUpData.fullName" title-input="Full Name" identity="fullName" type="text" placeholder="enter your full name..." />
+      <BaseInput v-model="signUpData.username" title-input="Username" identity="Username" type="username" placeholder="enter your username..." />
+      <BaseInput v-model="signUpData.email" title-input="Email" identity="email" type="email" placeholder="enter your email..." />
+      <div class="">
+        <BaseInput @keyInput="passwordCheck" v-model="signUpData.password" title-input="Password" identity="password" type="password" placeholder="enter your password..." />
+        <p class="text-danger mt-1 fw-medium text-sm" :style="{display: passwordStatusDisplay}">
+          The Password field must be at least 8 character
+        </p>
+      </div>
+      <div class="">
+        <BaseInput @keyInput="confirmationPasswordCheck" v-model="signUpData.confirmPassword" title-input="Confirm Password" identity="confirmPassword" type="password" placeholder="confirm your password..." />
+        <p class="text-danger mt-1 fw-medium text-sm" :style="{display: confirmPasswordFailed}">
+          The Password confirmation does not match
+        </p>
+        <p class="text-success mt-1 fw-medium text-sm" :style="{display: confirmPasswordSuccess}">
+          The Password confirmation does match
+        </p>
+      </div>
+      <BaseInput v-model="signUpData.address" title-input="Address" identity="address" type="text" placeholder="enter your address..." />
     </div>
-    <div class="">
-      <BaseInput @keyInput="confirmationPasswordCheck" v-model="signUpData.confirmPassword" title-input="Confirm Password" identity="confirmPassword" type="password" placeholder="confirm your password..." />
-      <p class="text-danger mt-1 fw-medium text-sm" :style="{display: confirmPasswordFailed}">
-        The Password confirmation does not match
-      </p>
-      <p class="text-success mt-1 fw-medium text-sm" :style="{display: confirmPasswordSuccess}">
-        The Password confirmation does match
-      </p>
-    </div>
-    <BaseInput v-model="signUpData.address" title-input="Address" identity="address" type="text" placeholder="enter your address..." />
-  </div>
-  <BaseButton btn-class="btn-main w-100 mt-5   py-2" btn-content="Login"/>
+    <BaseButton btn-class="btn-main w-100 mt-5 py-2" btn-content="Register"/>
+  </form>
+
   <p class="text-center mt-2">Have an account? <RouterLink class="main-color text-decoration-none" to="/auth/login">Sign in</RouterLink></p>
 </template>
 
